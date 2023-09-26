c760bb3abbbfda09a38f071fbd781adba3ed963b
Genty Gaylord <48088994+GaylordGenty@users.noreply.github.com>
2023-09-26 16:24:36 +0200

    ajout_question_personne

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,36 @@
-﻿public class Person {
+﻿using System;
 
-    public string Non {get;set};
+public class Person {
+
+    public string Nom {get;set};
     public string Prenom {get;set};
 
-    public Person(string non, string prenom) {
-        Non = non;
+    public Person(string nom, string prenom) {
+        Nom = nom;
         Prenom = prenom;
+        
     }
-
 }
+
+public static void Main() {
+
+    Person person1 = new Person(nom, prenom)
+
+    Console.WriteLine("Entrez votre noms")
+    person1.Nom = Console.Readline()
+
+    Console.WriteLine("Entrez votre prénoms")
+    person1.Prenom = Console.Readline()
+
+    Console.WriteLine("Nom : "+ person1.Nom +"\nPrénom :"+ person1.Prenom)
+    
+}
+       
+
+
+
+
+
+
 
     
