4b599c3d6ad7dfff75525f0284bb1b708df50597
LEGRAND Cyril <clegrand@arfp.asso.fr>
2023-09-27 10:17:46 +0200

    ajout de l'interface et de la classe PeopleContainer

--- src/Person.cs
+++ src/Person.cs
@@ -1,13 +1,13 @@
-namespace model {
+namespace src {
     public class Person {
 
-    public string Nom {get;set};
-    public string Prenom {get;set};
+    public string Nom { get; set; }
+    public string Prenom { get; set; }
 
-    public Person(string nom, string prenom) {
-        Nom = nom;
-        Prenom = prenom;
-        
+    public Person(string nom, string prenom) 
+        {
+            Nom = nom;
+            Prenom = prenom;  
+        }
     }
-}
 }
